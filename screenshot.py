23813f7b6d06f62b91458a1ee44952cb185bdef5
duduscript <duduscript@outlook.com>
2017-04-06 21:42:54 +0800

    screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -6,72 +6,71 @@ import time
 from PIL import Image
 
 def fullpage_screenshot(driver, file):
+    print("Starting chrome full page screenshot workaround ...")
+    
+    total_width = driver.execute_script("return document.body.offsetWidth")
+    total_height = driver.execute_script("return document.body.parentNode.scrollHeight")
+    viewport_width = driver.execute_script("return document.body.clientWidth")
+    viewport_height = driver.execute_script("return window.innerHeight")
+    print("Total: ({0}, {1}), Viewport: ({2},{3})".format(total_width, total_height,viewport_width,viewport_height))
+    rectangles = []
 
-        print("Starting chrome full page screenshot workaround ...")
+    i = 0
+    while i < total_height:
+        ii = 0
+        top_height = i + viewport_height
 
-        total_width = driver.execute_script("return document.body.offsetWidth")
-        total_height = driver.execute_script("return document.body.parentNode.scrollHeight")
-        viewport_width = driver.execute_script("return document.body.clientWidth")
-        viewport_height = driver.execute_script("return window.innerHeight")
-        print("Total: ({0}, {1}), Viewport: ({2},{3})".format(total_width, total_height,viewport_width,viewport_height))
-        rectangles = []
+        if top_height > total_height:
+            top_height = total_height
 
-        i = 0
-        while i < total_height:
-            ii = 0
-            top_height = i + viewport_height
+        while ii < total_width:
+            top_width = ii + viewport_width
 
-            if top_height > total_height:
-                top_height = total_height
+            if top_width > total_width:
+                top_width = total_width
+            
+            print("Appending rectangle ({0},{1},{2},{3})".format(ii, i, top_width, top_height))
+            rectangles.append((ii, i, top_width,top_height))
 
-            while ii < total_width:
-                top_width = ii + viewport_width
+            ii = ii + viewport_width
+        
+        i = i + viewport_height
 
-                if top_width > total_width:
-                    top_width = total_width
+    stitched_image = Image.new('RGB', (total_width, total_height))
+    previous = None
+    part = 0
 
-                print("Appending rectangle ({0},{1},{2},{3})".format(ii, i, top_width, top_height))
-                rectangles.append((ii, i, top_width,top_height))
+    for rectangle in rectangles:
+        if not previous is None:
+            driver.execute_script("window.scrollTo({0}, {1})".format(rectangle[0], rectangle[1]))
+            time.sleep(0.2)
+            #driver.execute_script("document.getElementById('topnav').setAttribute('style', 'position: absolute; top: 0px;');")
+            #time.sleep(0.2)
+            print("Scrolled To ({0},{1})".format(rectangle[0], rectangle[1]))
+            time.sleep(0.2)
 
-                ii = ii + viewport_width
+        file_name = "part_{0}.png".format(part)
+        print("Capturing {0} ...".format(file_name))
 
-            i = i + viewport_height
+        driver.get_screenshot_as_file(file_name)
+        screenshot = Image.open(file_name)
 
-        stitched_image = Image.new('RGB', (total_width, total_height))
-        previous = None
-        part = 0
+        if rectangle[1] + viewport_height > total_height:
+            offset = (rectangle[0], total_height - viewport_height)
+        else:
+            offset = (rectangle[0], rectangle[1])
 
-        for rectangle in rectangles:
-            if not previous is None:
-                driver.execute_script("window.scrollTo({0}, {1})".format(rectangle[0], rectangle[1]))
-                time.sleep(0.2)
-                #driver.execute_script("document.getElementById('topnav').setAttribute('style', 'position: absolute; top: 0px;');")
-                #time.sleep(0.2)
-                print("Scrolled To ({0},{1})".format(rectangle[0], rectangle[1]))
-                time.sleep(0.2)
+        print("Adding to stitched image with offset ({0}, {1})".format(offset[0],offset[1]))
+        stitched_image.paste(screenshot, offset)
 
-            file_name = "part_{0}.png".format(part)
-            print("Capturing {0} ...".format(file_name))
+        del screenshot
+        os.remove(file_name)
+        part = part + 1
+        previous = rectangle
 
-            driver.get_screenshot_as_file(file_name)
-            screenshot = Image.open(file_name)
-
-            if rectangle[1] + viewport_height > total_height:
-                offset = (rectangle[0], total_height - viewport_height)
-            else:
-                offset = (rectangle[0], rectangle[1])
-
-            print("Adding to stitched image with offset ({0}, {1})".format(offset[0],offset[1]))
-            stitched_image.paste(screenshot, offset)
-
-            del screenshot
-            os.remove(file_name)
-            part = part + 1
-            previous = rectangle
-
-        stitched_image.save(file)
-        print("Finishing chrome full page screenshot workaround...")
-        return True
+    stitched_image.save(file)
+    print("Finishing chrome full page screenshot workaround...")
+    return True
 
 def merge(img_paths,path):
     max_width,total_height = 0,0
